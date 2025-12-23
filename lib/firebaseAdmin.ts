32b9e3f1bc5d8e3ea8600ed004334a034e84b172
dylan patriarchi <dylanpat2004@icloud.com>
2025-12-23 17:50:50 +0100

    quick fix

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -13,7 +13,7 @@ if (!admin.apps.length) {
                     clientEmail,
                     privateKey: privateKey.replace(/\\n/g, '\n'),
                 }),
-                projectId: projectId, // Explicitly pass projectId
+                projectId: projectId,
                 storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
             });
             console.log("Firebase Admin initialized with explicit credentials.");
